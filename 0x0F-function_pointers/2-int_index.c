669c44153412d587699b14c4ecb431510b9cb367
Pbasilo <pbasilo@gmail.com>
2023-09-13 03:57:00 -0700

    2-int index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+/**
+ * int_index - return index place if comparison = true, else -1
+ * @array: array
+ * @size: size of elements in array
+ * @cm: pointer to function of one of the 3 in main 
+ * Return: 0
+ */
+int int_index(int *array, int size, int (*cm)(int))
+{
+	int i;
+	if (array == NULL || size <= 0 || cm == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cm(array[i]))
+			return (i);
+	}
+	return (-1);
+}
+	
