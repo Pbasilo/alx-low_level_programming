fd749db6668f4b3a0177f8df901e3343664f3f28
Pbasilo <pbasilo@gmail.com>
2023-09-28 03:55:01 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,12 +2,12 @@
 
 /**
  * get_bit - returns the value of a bit at an index in a decimal number
- * @n: number to search
+ * @r: number to search
  * @index: index of the bit
  *
  * Return: value of the bit
  */
-int get_bit(unsigned long int n, unsigned int index)
+int get_bit(unsigned long int r, unsigned int index)
 {
 	int bit_val;
 
